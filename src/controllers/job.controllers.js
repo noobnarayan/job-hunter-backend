fbd8558a0a068a03ada5320cb61498f82af409de
Narayan Das <workmail.narayan@gmail.com>
2024-03-04 20:53:05 +0530

    revert testing changes

--- src/controllers/job.controllers.js
+++ src/controllers/job.controllers.js
@@ -316,14 +316,10 @@ const saveJob = asyncHandler(async (req, res) => {
       .status(200)
       .json(new ApiResponse(200, {}, "Saved the job successfully"));
   } catch (error) {
-    return res
-      .status(500)
-      .send(
-        new ApiError(
-          500,
-          `An error occurred while saving the job : Error::${error}`
-        )
-      );
+    throw new ApiError(
+      500,
+      `An error occurred while saving the job : Error::${error}`
+    );
   }
 });
 
