24d8121f8dece53c0cbc34f2a70abce8e0228cf9
Narayan Das <workmail.narayan@gmail.com>
2024-03-05 07:03:01 +0530

    test fix: error handling middleware and error class modification

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -18,6 +18,16 @@ class ApiError extends Error {
       Error.captureStackTrace(this, this.constructor);
     }
   }
+
+  toJSON() {
+    return {
+      statusCode: this.statusCode,
+      message: this.message,
+      data: this.data,
+      success: this.success,
+      errors: this.errors,
+    };
+  }
 }
 
 export { ApiError };
